a062e521ce328b243cf7d074076e470ef290db17
Noah Alavi <rqh12@txstate.edu>
2025-04-19 10:31:13 -0500

    Erasing Player logic. Need to restart
    Need to add more helper functions, clarify purposes of mutexes and conditionals, and flow of player program.

--- Project2.c
+++ Project2.c
@@ -12,22 +12,18 @@ typedef struct Game {
     int greasy_card; //current greasy card
     int deck[52]; //ordered deck
     int deck_top; //index of "top" of deck (next card to draw)
+    int deck_bot; //index of last card. For discarding purposes.
     int curr_round; //current round #
     int curr_turn; //ID of player whose turn it is
     int total_rounds; // = num_players by defn.
     bool is_round_over; //signals if there was a preceding winner
     int round_winner; //-1 if no winner yet
     unsigned int seed;
+    int players_done; //tracks # of players finished with round.
     FILE *logfile; //needs to be opened and closed after running.
 
-    //mutexes and conditional. must be initialized and destroyed in main.
+    //FIXME add mutexes and conditional. must be initialized and destroyed in main.
     //Additionally, mutex ordering MUST be maintained. deck > bag > log > print...
-    pthread_mutex_t deck_mutex;
-    pthread_mutex_t bag_mutex;
-    pthread_mutex_t log_mutex;
-    pthread_mutex_t print_mutex;
-    pthread_mutex_t turn_mutex;
-    pthread_cond_t turn_cond;
 
     struct Player *players; //pointer to player array
 } Game;
@@ -59,11 +55,14 @@ int main(int argc, char *argv[]) {
     game.initial_bag = atoi(argv[3]);
     game.greasy_card = 0; 
     game.deck_top = 0;
+    game.deck_bottom = 52;
     game.curr_round = 0;
     game.curr_turn = 0;
     game.total_rounds = game.num_players;
     game.is_round_over = false;
     game.seed = atoi(argv[1]) + 1;
+    game.players_done = 0;
+
 
     //open logfile and error check
     game.logfile = fopen("logfile.txt", "w");
@@ -72,14 +71,8 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     
-    init_deck(&game);
+    //FIXME MUTEX initialization
 
-    pthread_mutex_init(&game.deck_mutex, NULL);
-    pthread_mutex_init(&game.bag_mutex, NULL);
-    pthread_mutex_init(&game.log_mutex, NULL);
-    pthread_mutex_init(&game.turn_mutex, NULL);
-    pthread_cond_init(&game.turn_cond, NULL);
-    pthread_mutex_init(&game.print_mutex, NULL);
 
     //Allocating player space and creating threads for each
     game.players = malloc(sizeof(Player) * game.num_players);
@@ -107,13 +100,7 @@ int main(int argc, char *argv[]) {
         pthread_join(threads[i], NULL);
     }
     
-    //After game finishes, destroy/close and exit:
-    pthread_mutex_destroy(&game.deck_mutex);
-    pthread_mutex_destroy(&game.bag_mutex);
-    pthread_mutex_destroy(&game.log_mutex);
-    pthread_mutex_destroy(&game.turn_mutex);
-    pthread_cond_destroy(&game.turn_cond);
-    pthread_mutex_destroy(&game.print_mutex);
+    //FIXME MUTEX DESTRUCTION
 
     fclose(game.logfile);
     free(game.players);
@@ -130,43 +117,11 @@ the “Greasy Card”
 (4) opening the first bag of potato chips, and
 (5) waiting for the round to end */
 void *player_func(void *arg) {
-    Player *p = (Player *)arg;
-    Game *game = p->game;
-
-    while(game->curr_round < game->total_rounds) {
-        //FIXME dealer logic. Must signal players
-        if (p->id == game->curr_round) {
-            //Shuffle deck and select greasy card from top.
-            pthread_mutex_lock(&game->deck_mutex);
-            shuffle_deck(game, &p->seed);
-
-            game->greasy_card = game->deck[game->deck_top];
-            game->deck_top++;
-            
-            pthread_mutex_lock(&game->log_mutex);
-            log_deck(game);
-            fprintf(game->logfile, "Greasy card is: %i\n", game->greasy_card);
-            pthread_mutex_unlock(&game->log_mutex);
-
-            deal_cards(game);
-
-            pthread_mutex_lock(&game->turn_mutex);
-            game->is_round_over = false;
-            game->round_winner = -1;
-            game->current_turn = (p->id + 1) % game->num_players;
-            pthread_cond_broadcast(&game->turn_cond);
-            pthread_mutex_unlock(&game->turn_mutex);
-            pthread_mutex_unlock(&game->deck_mutex);
-
-        }
-        break; //FIXME debugging
-        //normal player logic.
-        //print hand when it's their turn
-    }
-
-    return NULL;
+   //FIXME I'M BAD!!!!! Implement player logic
 }
 
+
+//FIXME add mutex locks TO the functions
 void init_deck(Game *game) {
     int k = 0;
     for (int i = 0; i < 4; i++) {
@@ -216,6 +171,6 @@ static inline unsigned int xorshift32(unsigned int *state)
 void deal_cards(Game* game) {
     for (int i = 0; i < game->num_players; i++) {
         game->players[i].hand = game->deck[game->deck_top];
-        game->deck_top++;
+        game->deck_top = (game->deck_top + 1) % 52;
     }
 }
